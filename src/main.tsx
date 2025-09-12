39cda946d134eef474b7c947eedcf1faf14fa888
jjitch <junnosuke_08@icloud.com>
2025-09-13 03:16:50 +0900

    introduce fluent provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
+import { FluentProvider, webDarkTheme } from "@fluentui/react-components";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webDarkTheme}>
+      <App />
+    </FluentProvider>
   </React.StrictMode>,
 );
